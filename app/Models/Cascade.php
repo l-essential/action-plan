685e2448d51b47cf89a57c49f90af408ee9eea28
Maika Kanaka <maika-kanaka@protonmail.com>
2020-12-16 08:58:24 +0700

    field dibuat oleh

--- app/Models/Cascade.php
+++ app/Models/Cascade.php
@@ -2,9 +2,25 @@
 
 use App\Models\Hris\MS_Department;
 use App\Models\Hris\MS_Karyawan;
+use App\User;
 use Illuminate\Database\Eloquent\Model;
 use TCG\Voyager\Traits\Translatable;
 
 class Cascade extends Model
 {
+
+    public function getCreatedByBrowseAttribute()
+    {
+        $objUser = new User;
+        $user = $objUser->find($this->created_by);
+        return !empty($user->name) ? $user->name : '';
+    }
+
+    public function getCreatedByReadAttribute()
+    {
+        $objUser = new User;
+        $user = $objUser->find($this->created_by);
+        return !empty($user->name) ? $user->name : '';
+    }
+
 }
